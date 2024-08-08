fd1cf776463aca69d1eb6ecd6c69201c49cc1282
anac2828 <anac2828@yahoo.com>
2024-08-08 10:44:43 -0700

    updated function

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,6 +1,6 @@
 // This is serverless function that lets you communication with the funa database
 
-exports.handler = async function (e) {
+exports.handler = async function (...args) {
   console.log(JSON.stringify(args, null, 2));
   // sub:free -- subscription type is free
   return JSON.stringify({ app_metadata: { roles: ['sub:free'] } });
