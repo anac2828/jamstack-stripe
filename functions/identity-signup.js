1322e49ed7077331a7266a91be273284221b583e
anac2828 <anac2828@yahoo.com>
2024-08-08 12:00:37 -0700

    wip: fixed signup function should now work correctly

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,7 +1,12 @@
 // This is serverless function that lets you communication with the funa database
 
-exports.handler = async function (...args) {
-  console.log(JSON.stringify(args, null, 2));
+exports.handler = async function (event) {
+  const user = JSON.parse(event.body);
+  console.log(JSON.stringify(user, null, 2));
+
   // sub:free -- subscription type is free
-  return JSON.stringify({ app_metadata: { roles: ['sub:free'] } });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ app_metadata: { roles: ['sub:free'] } }),
+  };
 };
