982f198ec9ba47101bdbad48b14477801c2a17da
anac2828 <anac2828@yahoo.com>
2024-08-08 18:10:29 -0700

    changed supabase key

--- functions/supabase.js
+++ functions/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseUrl = 'https://rvfqcjgttkcybnhkemjv.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2ZnFjamd0dGtjeWJuaGtlbWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxNTQxNDcsImV4cCI6MjAzODczMDE0N30.ebBOSQruOMr13e0H0j2x0GUUgd5txbRF4-gBTMIxwdw';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
