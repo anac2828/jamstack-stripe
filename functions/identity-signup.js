3f919d9cf643e1c0f579958e425026fc0f6a45a1
anac2828 <anac2828@yahoo.com>
2024-08-08 17:04:54 -0700

    wip: saving supabase user with netlify identity

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -4,6 +4,22 @@ exports.handler = async function (event) {
   const user = JSON.parse(event.body);
   console.log(JSON.stringify(user, null, 2));
 
+  const netlifyID = user.id;
+  const stripeID = 1;
+
+  // TODO create a customer record in supabase
+  const response = await fetch('https://nlovbzcjlodktqpwmpdu.supabase.co', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.SUPABASE_KEY}`,
+    },
+    body: JSON.stringify({ netlifyID, stripeID }),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(JSON.stringify(err, null, 2)));
+
+  console.log(response);
+
   // sub:free -- subscription type is free
   return {
     statusCode: 200,
