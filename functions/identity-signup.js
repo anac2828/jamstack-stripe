081787f76ab4f7b2e731a67b49c055f6e5ade9b1
anac2828 <anac2828@yahoo.com>
2024-08-08 18:01:40 -0700

    fixed user variable

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -3,7 +3,7 @@ import supabase from './supabase';
 // This is serverless function that lets you communication with the funa database
 
 exports.handler = async function (event) {
-  const user = JSON.parse(event.body);
+  const { user } = JSON.parse(event.body);
   console.log('USER', JSON.stringify(user, null, 2));
 
   const netlifyID = user.id;
@@ -14,7 +14,7 @@ exports.handler = async function (event) {
   const { data, error } = await supabase.from('User').insert([{ netlifyID, stripeID }]);
 
   // Did it work?
-  console.log('SUPABSE', data, error);
+  console.log('SUPABASE', data, error);
 
   // sub:free -- subscription type is free
   return {
