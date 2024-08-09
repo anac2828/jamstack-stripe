601a9d386348be6658b817d82d4f07e60980d605
anac2828 <anac2828@yahoo.com>
2024-08-08 17:27:07 -0700

    updated supase settings

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,24 +1,20 @@
+import supabase from './supabase';
+
 // This is serverless function that lets you communication with the funa database
 
 exports.handler = async function (event) {
   const user = JSON.parse(event.body);
-  console.log(JSON.stringify(user, null, 2));
+  console.log('USER', JSON.stringify(user, null, 2));
 
   const netlifyID = user.id;
   const stripeID = 1;
 
   // TODO create a customer record in supabase
-  const response = await fetch('https://nlovbzcjlodktqpwmpdu.supabase.co', {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${process.env.SUPABASE_KEY}`,
-    },
-    body: JSON.stringify({ netlifyID, stripeID }),
-  })
-    .then((res) => res.json())
-    .catch((err) => console.error(JSON.stringify(err, null, 2)));
+  // Insert a row
+  const { data, error } = await supabase.from('User').insert([{ netlifyID, stripeID }]);
 
-  console.log(response);
+  // Did it work?
+  console.log('SUPABSE', data, error);
 
   // sub:free -- subscription type is free
   return {
@@ -26,3 +22,6 @@ exports.handler = async function (event) {
     body: JSON.stringify({ app_metadata: { roles: ['sub:free'] } }),
   };
 };
+
+// Our standard serverless handler function
+exports.handler = async (event) => {};
