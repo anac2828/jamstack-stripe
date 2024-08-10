182dfa0d6ac90b6dbb1ade00b4636af7deff09fb
anac2828 <anac2828@yahoo.com>
2024-08-09 17:50:04 -0700

    fixed supabase.js function

--- functions/supabase.js
+++ functions/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseUrl = 'https://rvfqcjgttkcybnhkemjv.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2ZnFjamd0dGtjeWJuaGtlbWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxNTQxNDcsImV4cCI6MjAzODczMDE0N30.ebBOSQruOMr13e0H0j2x0GUUgd5txbRF4-gBTMIxwdw';
-const supabase = await createClient(supabaseUrl, supabaseKey);
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
