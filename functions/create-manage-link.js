860496d08c678627cdf44819fbd59a53cd9c4506
anac2828 <anac2828@yahoo.com>
2024-08-11 13:43:05 -0700

    wip: handle-webhooks

--- functions/create-manage-link.js
+++ functions/create-manage-link.js
@@ -3,19 +3,28 @@ import getUserId from './getUserId';
 
 // serverless function that creates a stripe customer portal link
 export const handler = async function (_event, context) {
-  //   User info comes from netlify identity
-  const { user } = context.clientContext;
-  // Get stripe ID
-  const { stripeID } = await getUserId('netlifyID', user.sub);
+  try {
+    //   User info comes from netlify identity
+    const { user } = context.clientContext;
 
-  // Create Stripe portal link
-  const link = await stripe.billingPortal.sessions.create({
-    customer: stripeID,
-    return_url: process.env.URL,
-  });
+    // Get stripe ID
+    const { stripeID } = await getUserId('netlifyID', user.sub);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(link.url),
-  };
+    // Create Stripe portal link
+    const link = await stripe.billingPortal.sessions.create({
+      customer: stripeID,
+      return_url: process.env.URL,
+    });
+
+    if (!link) {
+      throw new Error('Something when wrong');
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(link.url),
+    };
+  } catch (error) {
+    console.error(error);
+  }
 };
